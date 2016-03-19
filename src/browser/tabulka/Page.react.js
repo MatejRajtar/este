a95f9ffa76ea199c14fe5817164222f12fd495d0
matej <matej.rajtar@gmail.com>
2016-03-19 20:33:37 +0100

    Working search for the first column

--- src/browser/tabulka/Page.react.js
+++ src/browser/tabulka/Page.react.js
@@ -1,46 +1,30 @@
 import React, { Component, PropTypes } from 'react';
 import $ from 'jquery';
-import Immutable from 'immutable';
 
 class Table extends React.Component {
 
   constructor(props, context) {
     super(props, context);
-    this.state = {data: {}};
+    this.state = {
+      data: {},
+      filters: {}
+    };
   }
 
-  loadFromServer() {
+  componentDidMount() {
     $.ajax({
       url: 'http://www.json-generator.com/api/json/get/cikrApwlgy?indent=2',
       dataType: 'json',
       success: (data) => {
-        this.setState({data: data});
+        this.setState({
+          data: data,
+          heading: data[1]
+        });
       },
       error: (xhr, status, err) => {
         console.error(this.props.url, status, err.toString());
       }
     });
-  }
-
-  componentDidMount() {
-    this.loadFromServer();
-  }
-
-  objToTable(obj) {
-    if (obj[0]) {
-      return (
-        <table>
-          {this.renderHeaderByKeys(Object.keys(obj[0]))}
-          <tbody>
-            <tr style={this.constructor.styles.tr}>{
-              Object.keys(obj).map((key, i) => {
-                return this.renderTd(Object.values(obj[i]), i);
-              })
-            }</tr>
-          </tbody>
-        </table>
-      );
-    }
   }
 
   renderHeaderByKeys(keys) {
@@ -52,9 +36,15 @@ class Table extends React.Component {
             <th style={this.constructor.styles.th} key={i}>
               {key}
               <div></div>
-              <input id={key} type="text" style={this.constructor.styles.input} />
+              <input
+                id={i}
+                type="text"
+                onChange={
+                  this.filterData.bind(this)
+                  }
+                style={this.constructor.styles.input}
+              />
             </th>
-
           );
         })
       }</tr>
@@ -62,9 +52,42 @@ class Table extends React.Component {
     );
   }
 
+  filterData(phrase) {
+    event.preventDefault();
+    if (phrase.target.value != "") {
+      this.state.filters[phrase.target.id] = phrase.target.value;
+    }
+    else {
+      delete this.state.filters[phrase.target.id];
+    }
+    this.forceUpdate()
+  }
+
+  objToTable(obj) {
+    return (
+      <tbody>{
+        Object.keys(obj).map((key, i) => {
+          var j = 0;
+          console.log(key);
+          /*Filter only if there are filters in the dictionary this.state.filters*/
+          if (Object.keys(this.state.filters).length > 0) {
+            for (j=0; j < Object.keys(this.state.filters).length; j++) {
+              if (!(((Object.values(obj[i])[j]).toString()).indexOf((Object.values(this.state.filters)[j]).toString()))) {
+                return this.renderTd(Object.values(obj[i]), i);
+              }
+            }
+          }
+          else {
+            return this.renderTd(Object.values(obj[i]), i);
+          }
+        })
+      }</tbody>
+    );
+  }
+
   renderTd(keys, i) {
     return (
-      <tr >{
+      <tr style={this.constructor.styles.tr}>{
         keys.map((key, i) => {
           return (
             <td style={this.constructor.styles.td} key={i}>
@@ -77,13 +100,19 @@ class Table extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-
-        {this.objToTable(this.state.data)}
-
-      </div>
-    );
+    if (this.state.heading) {
+      return (
+        <div>
+          <table>
+            {this.renderHeaderByKeys(Object.keys(this.state.heading))}
+            {this.objToTable(this.state.data)}
+          </table>
+        </div>
+      );
+    }
+    else {
+      return (<div>Loading data...</div>)
+    }
   }
 
 }
@@ -95,15 +124,14 @@ Table.styles = {
   },
   th: {
     backgroundColor: "#4CAF50",
-    color : "white",
+    color: "white",
     padding: "6px",
     fontWeight: "bold",
     border: "1px solid #cccccc",
-    width: "100%"
   },
   input: {
     width: "100%",
-    color: "black"
+    color: "#4CAF50"
   }
 };
 
